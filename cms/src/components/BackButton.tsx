c3101eeed8f6e9e8a7e89975623f90aeb651bbfb
Piguins <piguins.github@gmail.com>
2025-12-05 10:05:53 +0700

    Simplify BackButton component - just display 'Back' text

--- cms/src/components/BackButton.tsx
+++ cms/src/components/BackButton.tsx
@@ -6,10 +6,10 @@ import './BackButton.css'
 
 interface BackButtonProps {
   href: string
-  children: React.ReactNode
+  children?: React.ReactNode
 }
 
-export default function BackButton({ href, children }: BackButtonProps) {
+export default function BackButton({ href, children = 'Back' }: BackButtonProps) {
   return (
     <Link href={href}>
       <motion.button
@@ -17,9 +17,6 @@ export default function BackButton({ href, children }: BackButtonProps) {
         whileHover={{ scale: 1.02 }}
         whileTap={{ scale: 0.98 }}
       >
-        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-          <path d="M19 12H5M12 19l-7-7 7-7" />
-        </svg>
         <span>{children}</span>
       </motion.button>
     </Link>
